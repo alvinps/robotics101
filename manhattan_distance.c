089ced65ec8752e268f792ad5a0e167aeed75fa1
UnclePhiI <57052197+UnclePhiI@users.noreply.github.com>
2020-12-13 08:20:10 -0600

    Now generates the manhattan distance grid
    In case this function is to be used with the final exam review to check the manhattan distance problem

--- manhattan_distance.c
+++ manhattan_distance.c
@@ -1,7 +1,7 @@
-// C Program to find sum of Manhattan distance 
+// CPP Program to find sum of Manhattan distance 
 // between all the pairs of given points 
 #include <stdio.h> 
-
+#include <math.h>
   
 // Return the sum of distance between all 
 // the pair of points of a n array
@@ -27,25 +27,24 @@ int main()
 
    // printf("%d",distancesum(x,y,n)); 
 
-    int max_x = 13;
-    int max_y = 11;
+    int max_x = 12;
+    int max_y = 10;
 
     int goal_x = 5;
-    int goal_y = 5;
+    int goal_y = 6;
 
-    printf("x_goal: %d\ny_goal:%d\n",goal_x,goal_y);
-    int weight[12][10];
-    for(int i=0;i<max_x;i++)
+    int weight[max_x][max_y];
+    for(int i=0;i<max_y;i++)
     {
-        for (int j=0;j<max_y;j++)
+        for (int j=0;j<max_x;j++)
         {
             weight[i][j] = -1;
         }
       }
 
-      for(int i=0;i<max_x;i++)
+      for(int i=0;i<max_y;i++)
     {
-        for (int j=0;j<max_y;j++)
+        for (int j=0;j<max_x;j++)
         {
             //at point 0,0 of the array need to make it max y but min x and then iterte throughout
             //the 2-d array updating the weights accordingly
@@ -55,14 +54,14 @@ int main()
             weight[i][j] = distancesum(x,y,n);
         }
       }
-      for(int i=0;i<max_x;i++)
+      for(int i=0;i<max_y;i++)
     {
-        for (int j=0;j<max_y;j++)
+        for (int j=0;j<max_x;j++)
         {
             //at point 0,0 of the array need to make it max y but min x and then iterte throughout
             //the 2-d array updating the weights accordingly
         
-            printf(" %4d ",weight[i][j]);
+            printf("%d\t",weight[i][j]);
         }
         printf("\n");
       }
